df4ff12162024a160a8efe205824143dcc936034
Raiza Campos <raizacampos2@gmail.com>
2018-09-08 19:25:32 -0300

    funções da tela aluno e do botão aluno na tela inicial

--- app/src/main/java/com/example/raiza/myapplication/AlunoActivity.java
+++ app/src/main/java/com/example/raiza/myapplication/AlunoActivity.java
@@ -1,13 +1,35 @@
 package com.example.raiza.myapplication;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AlunoActivity extends AppCompatActivity {
+
+    private EditText edtNome, edtMatricula;
+    private Button btnOk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aluno);
+        edtNome = (EditText)findViewById(R.id.edt_aluno_nome);
+        edtMatricula = (EditText)findViewById(R.id.edt_aluno_matricula);
+        btnOk = (Button)findViewById(R.id.btn_aluno_ok);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent resultado = new Intent();
+                resultado.putExtra(MainActivity.ALUNO_NOME, edtNome.getText().toString());
+                resultado.putExtra(MainActivity.ALUNO_MATRICULA, edtNome.getText().toString());
+                setResult(Activity.RESULT_OK, resultado);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/raiza/myapplication/MainActivity.java
+++ app/src/main/java/com/example/raiza/myapplication/MainActivity.java
@@ -2,35 +2,32 @@ package com.example.raiza.myapplication;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String PESSOA_NOME = "nome";
-    private static final int REQUEST_SIAPE = 1;
-    public static final String PROF_SIAPE = "siape";
-    private Button btnProfessor;
-    private EditText edtNome;
+    private static final int REQUEST_ALUNO = 1;
+    public static final String ALUNO_NOME = "nome aluno";
+    public static final String ALUNO_MATRICULA = "matricula aluno";
     private TextView txtMensagem;
+    private Button btnAluno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtMensagem = (TextView)findViewById(R.id.txt_mensagem);
-        edtNome = (EditText) findViewById(R.id.edt_nome);
 
-        btnProfessor = findViewById(R.id.btn_professor);
-        btnProfessor.setOnClickListener(new View.OnClickListener() {
+        btnAluno = findViewById(R.id.btn_aluno);
+        btnAluno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AlunoActivity.class);
-                intent.putExtra(PESSOA_NOME, edtNome.getText().toString());
-                startActivityForResult(intent, MainActivity.REQUEST_SIAPE);
+                startActivityForResult(intent, MainActivity.REQUEST_ALUNO);
             }
         });
 
@@ -40,10 +37,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == MainActivity.REQUEST_SIAPE && resultCode == Activity.RESULT_OK && data != null) {
+        if(requestCode == MainActivity.REQUEST_ALUNO && resultCode == Activity.RESULT_OK && data != null) {
             Bundle bundleResultado = data.getExtras();
-            String siape = bundleResultado.getString(MainActivity.PROF_SIAPE);
-            txtMensagem.setText("Olá professor de SIAPE: " + siape);
+            String nome = bundleResultado.getString(MainActivity.ALUNO_NOME);
+            String matricula = bundleResultado.getString(MainActivity.ALUNO_MATRICULA);
+            txtMensagem.setText("Olá" + nome + "de matrícula: " + matricula);
         }
     }
 }
